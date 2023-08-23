621224b5a5ef2b0c25f5583f6991ab2cd2c81590
Shaja Nasir <Shajanasir@gmail.com>
2023-08-24 02:31:24 +0530

    Add Header and switch branch to header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useState } from "react";
 import "./App.css";
-
-function App() {
-  return <p>Todo application</p>;
-}
+import Header from "./components/Header";
+const initialTodos = [
+  {
+    id: Math.random().toString(),
+    title: "Day 01",
+    description: "Have to go to colombo",
+    isFav: false,
+    isDone: false,
+  },
+  {
+    id: Math.random().toString(),
+    title: "Day 02",
+    description: "Have to go to Kandy",
+    isFav: false,
+    isDone: false,
+  },
+];
+const App = () => {
+  const [todos, setTodos] = useState(initialTodos);
+  return (
+    <div className='app'>
+      <Header />
+      <main>Body</main>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import classes from "./Header.module.css";
+const Header = (propsclear
+    ) => {
+  return (
+    <div>
+      <p>Todo application name</p>
+      <p>Add Todo</p>
+      <p>Favorites</p>
+      <p>Login/ Logout</p>
+    </div>
+  );
+};
+
+export default Header;
