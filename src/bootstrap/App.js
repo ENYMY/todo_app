719dc5bb017934f739e7486944cf8d2213128342
Mohamed Nasir Mohamed Shaja <shaja.n@intervest.lk>
2023-09-27 12:31:54 +0530

    add todo template

--- src/bootstrap/App.js
+++ src/bootstrap/App.js
@@ -1,29 +1,15 @@
-import Header from "../components/Header";
-import { Grid } from "@mui/material";
-import TodoList from "../components/todo/TodoList";
-import NewTodo from "../components/todo/NewTodo";
 import { ErrorBoundary } from "../components/ui-elements/common/ErrorBoundary";
 import { ContextProvider } from "./Provider";
+import TodoTemplate from "../components/ui-elements/template/Todo.template";
+import TodoPage from "../components/module/todo/TodoPage";
 
 const App = () => {
   return (
     <ErrorBoundary>
       <ContextProvider>
-        <Grid container direction="column" spacing={2}>
-          <Grid item xs={12}>
-            <Header />
-          </Grid>
-          <Grid xs={12} item sx={{ marginLeft: "10px" }}>
-            <Grid container spacing={1} direction="row">
-              <Grid item xs={4}>
-                <NewTodo />
-              </Grid>
-              <Grid item xs={8}>
-                <TodoList />
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
+        <TodoTemplate>
+          <TodoPage />
+        </TodoTemplate>
       </ContextProvider>
     </ErrorBoundary>
   );
